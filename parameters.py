0124ad37ae83870fd41ba6e3ff2db0c26741c766
Dokyung Lee <dokylee54@gmail.com>
2019-12-30 19:53:58 +0900

    parameters

---
+++ parameters.py
@@ -0,0 +1,5 @@
+profile_imgURL = 'https://ibb.co/0qS77k3'
+
+subscription_key = "bdc06bf4e1b543aaa8e378e67d4c792b" 
+
+images_to_compare_path = "/Users/dokylee/Desktop/dokylee/girlsinICT/dev/face_similarity/face_images/"
